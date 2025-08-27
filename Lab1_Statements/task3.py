116f4422ed9b65c112a4990425fefea2f6ed17f6
Prateek <pkanna@murraystat.edu>
2025-08-27 10:10:30 -0500

    Done with task 3

---
+++ Lab1_Statements/task3.py
@@ -0,0 +1,11 @@
+name = input('What is your name?')
+if len(name) > 0:
+    print('Hello ' , name)
+else:
+    print('Enter your name, please!')
+
+    name = input('What is your name?')
+if len('first name, last name') > 0:
+    print('Hello ' , name)
+else:
+    print('Enter your name, please!')
