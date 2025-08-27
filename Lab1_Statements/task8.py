f3738d40910177a49e20b1f78a52c35a20769178
Prateek <pkanna@murraystat.edu>
2025-08-27 11:45:54 -0500

    Done with task 8

--- Lab1_Statements/task8.py
+++ Lab1_Statements/task8.py
@@ -2,6 +2,16 @@ alphabets='abcdefghijklmnopqrstuvwxyz'
 while True:
     user_entry = input('Enter a letter (or blank to exit): ')
     if user_entry == '':
+        break
     else:
         position = alphabets.find(user_entry)
-        print(if'{user_entry is at position {position}')
+        print(f'{user_entry} is at position {position}')
+
+alphabets='abcdefghijklmnopqrstuvwxyz'
+while True:
+    user_entry = input('Enter a letter (or blank to exit): ')
+    if user_entry == '':
+        break
+    else:
+        position = alphabets.find(user_entry)
+        print(f'{user_entry} is at position {position}')
