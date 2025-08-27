37c68b51f2f9400de37de3fdaec357e728fd97ab
Prateek <pkanna@murraystat.edu>
2025-08-27 10:55:41 -0500

    Done with task 2

--- Lab1_Statements/task2.py
+++ Lab1_Statements/task2.py
@@ -3,7 +3,7 @@ price = float(input('Enter price per bag'))
 print('Total cost: , quantity * price')
 print(price*quantity)
 
-quantity = int(input('Enter number of miles driven'))
-price = float(input('Enter gallons used per mile'))
-print('Total cost: , miles * gallons')
-print(price*quantity)
+miles_driven = int(input('Enter miles driven: '))
+gallons_used = float(input('Enter gallons used: '))
+miles_per_gallon = miles_driven / gallons_used
+print(f'Miles per gallon is {miles_per_gallon}')
