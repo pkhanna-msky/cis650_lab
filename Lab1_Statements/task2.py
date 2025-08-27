a3ac36096c635cbed7df7787d159602c5c5f3393
Prateek <pkanna@murraystat.edu>
2025-08-27 10:59:27 -0500

    Done with task 2

--- Lab1_Statements/task2.py
+++ Lab1_Statements/task2.py
@@ -6,4 +6,4 @@ print(price*quantity)
 miles_driven = int(input('Enter miles driven: '))
 gallons_used = float(input('Enter gallons used: '))
 miles_per_gallon = miles_driven / gallons_used
-print(f'Miles per gallon is {miles_per_gallon}')
+print(f'Miles per gallon is {miles_per_gallon:d}')
