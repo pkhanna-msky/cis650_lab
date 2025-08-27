cffce6289981111ee466952eac69911f6d668d5f
Prateek <pkanna@murraystat.edu>
2025-08-27 10:20:50 -0500

    Done with task 5

---
+++ Lab1_Statements/task4.py
@@ -0,0 +1,11 @@
+quantity = input('Enter quantity:')
+if quantity.isdigit():
+    quantity = input(quantity)
+else:
+    print('Invalid quantity. Please enter number.')
+
+    quantity = input('Enter quantity:')
+if quantity.isnondigit():
+    quantity = input(quantity)
+else:
+    print('Invalid quantity. Please enter number.')
---
+++ Lab1_Statements/task5.py
@@ -0,0 +1,20 @@
+number1 = int(input('Enter first number: '))
+number2 = int(input('Enter second number: '))
+if number1 == number2:
+    result = 'Equal'
+elif number1<number2:
+    result = 'First is less than second'
+else:
+    result = 'first is greater than second'
+    print(result)
+
+number1 = int(input('Enter first number: '))
+number2 = int(input('Enter second number: '))
+if number1 == number2:
+    result = 'larger number'
+elif number1<number2:
+    result = 'First is less than second'
+else:
+    result = 'first is greater than second'
+    print(result)
+  
